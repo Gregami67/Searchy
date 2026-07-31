fea751bb31dfaff849a44a4c03b7e60d2777e828
Gregami <git@gregami.com>
2026-07-31 13:37:26 -0400

    refactor: Docker compose port and valkey_data

--- src/wsgi.py
+++ src/wsgi.py
@@ -4,10 +4,10 @@ from searchy.attributes import DEBUG
 def run_dev():
     from app import app
 
-    app.run(debug=DEBUG, host="0.0.0.0", use_reloader=False)
+    app.run(debug=DEBUG, host="0.0.0.0", port=5000, use_reloader=False)
 
 
 def run_prod():
     import subprocess
 
-    subprocess.run(["gunicorn", "-b", "0.0.0.0", "app:app"])
+    subprocess.run(["gunicorn", "-b", "0.0.0.0:5000", "app:app"])
