2eefe156e94dbcc02fd3349bede20c675e18c068
Gregami <git@gregami.com>
2026-08-05 00:20:32 -0400

    fix: Store thumbs in volume

--- src/searchy/config.py
+++ src/searchy/config.py
@@ -4,12 +4,9 @@ from pathlib import Path
 DEBUG = True if os.getenv("DEBUG") == "true" else False
 WATCHDOG = True if os.getenv("WATCHDOG") == "true" else False
 
-BASE_DIR = Path(__file__).resolve().parent
-
-THUMB_DIR = BASE_DIR / "thumbs"
-THUMB_DIR.mkdir(parents=True, exist_ok=True)
-
 IMAGE_DIR = os.getenv("IMAGE_DIR")
+THUMB_DIR = Path(os.getenv("THUMB_DIR"))
+THUMB_DIR.mkdir(exist_ok=True)
 
 BATCH_SIZE = int(os.getenv("BATCH_SIZE", 32))
 IMAGE_COUNT = int(os.getenv("IMAGE_COUNT", 50))
